bff810ceac895c9fe8b28e54dd2c5fe6d31c7aa4
kate deng <katedeng2020@gmail.com>
2023-08-23 13:46:03 -0600

    button & alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
+import Button from './components/Button';
+import { useState } from 'react';
 
 function App() {
-	const items = [ 'New York', 'San Francisco', 'Tokyo', 'London', 'Paris' ];
-	
-	const handleSelectItem = (item: string) => {
-		console.log(item)
-	}
+	const [ alertVisible, setAlertVisible ] = useState(false);
+
 	return (
 		<div>
-			<ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-			<Alert>
-				Hello,<span> God!</span>
-			</Alert>
+			{alertVisible && <Alert onClose={() => setAlertVisible(false)}>My alert</Alert>}
+			<Button color="primary" onClick={() => setAlertVisible(ture)}>
+				my Button
+			</Button>
 		</div>
 	);
 }
